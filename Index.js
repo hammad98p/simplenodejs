ac274a1c585c5a90e406710f4a9f7885c0e9d3ea
hammad98p <126167448+hammad98p@users.noreply.github.com>
2023-02-23 18:55:36 +0500

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,42 @@
+
+const port = 8000;
+const hostname = "localhost";
+
+const http = require('http');
+const fs = require('fs');
+
+
+const express = require('Express');
+const app = express();
+
+
+// const server = http.createServer((req, res) => {
+//     res.end('Server Is Started');
+// })
+
+const hehe = express.urlencoded(
+    // {extended: true}
+);
+app.use(hehe);
+app.get('/', (req,res)=> {
+
+    res.setHeader('Content-Type', 'text/html');
+    res.end(fs.readFileSync('html.html'));
+
+})
+app.post('/', (req, res)=>{
+
+    console.log(req.body);
+    res.end(`${req.body.name}, i welcome you to my webpage`);
+
+})
+app.get('/hello', (req,res)=> {
+
+    res.end('you requested hello.html');
+
+})
+
+
+app.listen(port, hostname, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
